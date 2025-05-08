7b6c9a4104a863c288af4c4c45881c0682a8270a
JoseB91 <jose.briones.r@hotmail.com>
2025-05-08 17:07:12 -0500

    Initial Commit

---
+++ TVMaze/TVMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeApp.swift
+//  TVMaze
+//
+//  Created by José Briones on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
